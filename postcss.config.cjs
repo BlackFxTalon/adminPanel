46e4c5195bf85477b0a4670227c1b05a12325cb9
BlackFxTalon <andyv@inbox.ru>
2022-11-15 15:50:11 +0500

    changes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,41 +1,41 @@
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
 const postcssPresetEnv = require('postcss-preset-env');
-const stylelint = require('stylelint');
+// const stylelint = require('stylelint');
 const cssnano = require('cssnano');
 
 module.exports = {
     plugins: [
-       stylelint(
-        {
-        extends:"stylelint-config-recommended-scss",
-        rules: {
-          "font-family-name-quotes": "always-where-required",
-          "function-url-no-scheme-relative": true,
-          "function-url-quotes": "always",
-          "string-quotes": "double",
-          "value-keyword-case": [
-            "lower",
-            {
-              "ignoreKeywords":["optimizeSpeed"],
-            }
-          ],
-          "unit-disallowed-list": [],
-          "max-empty-lines": 2,
-          "no-descending-specificity": true,
-          "no-duplicate-selectors": true,
-          "font-family-no-missing-generic-family-keyword": null,
-          "property-no-unknown": [
-            true,
-            {
-              "ignoreProperties": ["/^lost-/"]
-            }
-          ],
-          "selector-pseudo-class-no-unknown": [true, { "ignorePseudoClasses": ["global"] }],
-        },
-        ignoreFiles: ["node_modules/*"],
-        defaultSeverity: "error",
-        customSyntax: "postcss-html"
-       }),
+      //  stylelint(
+      //   {
+      //   extends:"stylelint-config-recommended-scss",
+      //   rules: {
+      //     "font-family-name-quotes": "always-where-required",
+      //     "function-url-no-scheme-relative": true,
+      //     "function-url-quotes": "always",
+      //     "string-quotes": "double",
+      //     "value-keyword-case": [
+      //       "lower",
+      //       {
+      //         "ignoreKeywords":["optimizeSpeed"],
+      //       }
+      //     ],
+      //     "unit-disallowed-list": [],
+      //     "max-empty-lines": 2,
+      //     "no-descending-specificity": true,
+      //     "no-duplicate-selectors": true,
+      //     "font-family-no-missing-generic-family-keyword": null,
+      //     "property-no-unknown": [
+      //       true,
+      //       {
+      //         "ignoreProperties": ["/^lost-/"]
+      //       }
+      //     ],
+      //     "selector-pseudo-class-no-unknown": [true, { "ignorePseudoClasses": ["global"] }],
+      //   },
+      //   ignoreFiles: ["node_modules/*"],
+      //   defaultSeverity: "error",
+      //   customSyntax: "postcss-html"
+      //  }),
        postcssFlexbugsFixes({}),
        postcssPresetEnv(
         {
@@ -44,7 +44,7 @@ module.exports = {
             grid: "autoplace"
           },
           stage: 3,
-          browsers: ["last 2 versions", "ios >= 8"]
+          browsers: ["last 2 versions", "not dead", "ios >= 8"]
        }
        ),
        cssnano({
